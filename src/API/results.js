5ccdd386392f1dc3f70806286834d01e2e957780
nadinem100 <67973324+nadinem100@users.noreply.github.com>
2021-02-06 13:17:05 -0500

    matches page

---
+++ src/API/results.js
@@ -0,0 +1,22 @@
+const store1 = {
+  name: 'Chipotle', 
+  location: 'Penn', 
+  desc: 'desc', 
+  price: '$'
+}
+
+const store2 = {
+  name: 'Bob', 
+  location: 'Penn', 
+  desc: 'desc2', 
+  price: '$$'
+}
+
+const store3 = {
+  name: 'Jo', 
+  location: 'Penn', 
+  desc: 'desc3', 
+  price: '$$$'
+}
+
+export const results = [store1, store2, store3]
