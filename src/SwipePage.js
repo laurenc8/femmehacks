736d0e07703dbcf82919190ac95f2d2c5920b709
Naomi Kenyatta <naomikenyatta@dhcp-10-250-46-21.harvard.edu>
2021-02-06 18:35:56 -0500

    idk

--- src/SwipePage.js
+++ src/SwipePage.js
@@ -1,7 +1,17 @@
+
+import RestaurantCard from './components/RestaurantCard'
+import Chip from './Assests/Chipotle.jpg'
+
 function SwipePage() {
   return (
     <div>
       Swipe!
+
+      <RestaurantCard 
+                  name = "Chipotle"
+                  desc = "Fast-food chain offering Mexican fare, including design-your-own burritos, tacos and bowls."
+                  price = "$"
+                />
     </div>
   );
 }
