5026cb314e79a12f84b0b4d89e2ae13aac7e3254
laurenc8 <laurenc8822@gmail.com>
2021-02-05 22:49:04 -0500

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Redirect, Link } from 'react-router-dom';
+import SwipePage from './SwipePage';
+import MatchesPage from './MatchesPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/" component={SwipePage} />
+        <Route exact path="/matches" component={MatchesPage} />
+        <Route component={() => <Redirect to="/" />} />
+      </Switch>
     </div>
   );
 }
---
+++ src/MatchesPage.js
@@ -0,0 +1,9 @@
+function MatchesPage() {
+  return (
+    <div>
+      Matches!
+    </div>
+  );
+}
+
+export default MatchesPage;
