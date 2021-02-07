ea3f9177ef90c93ff684891677a415c5c2468284
nadinem100 <67973324+nadinem100@users.noreply.github.com>
2021-02-06 20:41:21 -0500

    api

--- src/API/results.js
+++ src/API/results.js
@@ -1,24 +1,24 @@
 const store1 = {
-  name: 'Chipotle', 
-  location: 'Penn', 
-  desc: 'This is a really good restaurant, and you should totally come here. We have lots of different food!', 
-  price: '$',
-  reviews: 'thisi s really good'
+  name: 'China Inn Restaurant', 
+  location: 'Pennsylvania', 
+  desc: 'www.chinainnrestaurant.com', 
+  price: '$$',
+  reviews: 'My family has eaten here about once a week since we moved down the street about 4 years ago. The food is always delicious, pickup is fast, and eating at the restaurant itself is a real treat. The people who work there are very kind. I\'ve never eaten a bad item from the menu. I would recommend the sesame chicken, edamame, spring rolls, lo mein, and most delicious and unique the SESAME BALLS.'
 }
 
 const store2 = {
-  name: 'Bob', 
-  location: 'Penn', 
-  desc: 'This is a really good restaurant, and you should totally come here. We have lots of different food!This is a really good restaurant, and you should totally come here. We have lots of different food!', 
-  price: '$$',
-  reviews: 'REVIEWIREWIRJEWI'
+  name: 'New Number One', 
+  location: 'Pennsylvania', 
+  desc: 'https://www.newnumberonephilly.com/', 
+  price: '$$$',
+  reviews: 'I actually loved their dishes and service, excellent location  to where I live. The staff members are always friendly. Will come back again.'
 }
 
 const store3 = {
-  name: 'Jo', 
-  location: 'Penn', 
-  desc: 'This is a really good restaurant, and you should totally come here. We have lots of different food!', 
-  price: '$$$',
+  name: 'Rising Sun China Restaurant', 
+  location: 'Pennsylvania', 
+  desc: '', 
+  price: '$',
   reviews: 'PLEPSLPLASEEE'
 }
 // const results = [store1, store2, store3]
